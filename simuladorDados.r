d2e64c98401f384be86d012afff83d3da74c8ddb
lucas-satoshi <lucas.satoshi.18@gmail.com>
2025-06-15 22:55:34 -0300

    new function update

--- simuladorDados.r
+++ simuladorDados.r
@@ -5,58 +5,80 @@
 
 #autor: Lucas Satoshi Cipriano Oikawa
 
-#data: 2025-03-08
+#data: 2025-06-15
 
 #contato: 2226364@aluno.univesp.br
 
 #notas: essa função ainda está em fase de desenvolvimento e algumas funcionali-
-#dades ainda não foram testadas ou podem sofrer alterações. 
+#dades ainda não foram testadas ou podem sofrer alterações.
 ################################################################################
 
-cat("Bem-vindo ao Simulador de Geração de Novas Progênies\n\n")
-cat("Por favor, insira o nome do arquivo CSV contendo os cromossomos do pai (2 colunas):\n")
-arquivo_pai <- readline()
-cat("Por favor, insira o nome do arquivo CSV contendo os cromossomos da mãe (2 colunas):\n")
-arquivo_mae <- readline()
-
-# Leitura dos dados
-dados_pai <- read.csv(arquivo_pai, header = TRUE, sep = ";", stringsAsFactors = FALSE)
-dados_mae <- read.csv(arquivo_mae, header = TRUE, sep = ";", stringsAsFactors = FALSE)
-
-# Verificação de consistência
-if(ncol(dados_pai) != 2 || ncol(dados_mae) != 2) {
-  stop("Ambos arquivos devem conter exatamente 2 colunas")
-}
-
-if(nrow(dados_pai) != nrow(dados_mae)) {
-  stop("Os arquivos devem ter o mesmo número de linhas")
-}
-
-# Processamento
-resultado <- data.frame(
-  Cromossomo_Pai = character(),
-  Cromossomo_Mae = character(),
-  stringsAsFactors = FALSE
-)
-
-for(i in 1:nrow(dados_pai)) {
-  # Seleção aleatória do cromossomo paterno
-  crom_pai <- sample(dados_pai[i, ], 1)
+# Função para simular a geração de novas progênies
+simular_progênie <- function(df_pai, df_mae, num_individuos) {
+  # Verificação de consistência
+  if(ncol(df_pai) != 2 || ncol(df_mae) != 2) {
+    stop("Ambos data frames devem conter exatamente 2 colunas")
+  }
   
-  # Seleção aleatória do cromossomo materno
-  crom_mae <- sample(dados_mae[i, ], 1)
+  if(nrow(df_pai) != nrow(df_mae)) {
+    stop("Os data frames devem ter o mesmo número de linhas")
+  }
   
-  # Armazenamento do resultado
-  resultado[i, ] <- c(crom_pai, crom_mae)
+  # Lista para armazenar os cromossomos selecionados para cada indivíduo
+  # Cada elemento da lista será um vetor de cromossomos (pai ou mãe) para um indivíduo
+  colunas_para_df_final <- list()
+  
+  # Definindo uma semente para reprodutibilidade
+  set.seed(123) # Pode ser alterado para um valor dinâmico como as.integer(Sys.time())
+  
+  for(j in 1:num_individuos) {
+    # Seleção aleatória de uma das duas colunas (cromossomos inteiros) do pai
+    selected_col_pai_index <- sample(1:ncol(df_pai), 1)
+    selected_chromosome_pai <- df_pai[[selected_col_pai_index]]
+    
+    # Seleção aleatória de uma das duas colunas (cromossomos inteiros) da mãe
+    selected_col_mae_index <- sample(1:ncol(df_mae), 1)
+    selected_chromosome_mae <- df_mae[[selected_col_mae_index]]
+    
+    # Adiciona os cromossomos selecionados como novas colunas à lista
+    colunas_para_df_final[[paste0("Individuo_", j, "_Pai")]] <- selected_chromosome_pai
+    colunas_para_df_final[[paste0("Individuo_", j, "_Mae")]] <- selected_chromosome_mae
+  }
+  
+  # Combina todos os vetores da lista em um único data frame por colunas
+  resultado_final <- as.data.frame(colunas_para_df_final, stringsAsFactors = FALSE)
+  
+  return(resultado_final)
 }
 
-# Salvamento do resultado
-write.table(
-  resultado,
-  file = "progenie_selecionada.csv",
-  sep = ";",
-  row.names = FALSE,
-  quote = FALSE
-)
+# Exemplo de uso da função (para demonstração, pode ser removido ou adaptado)
 
-cat("\nSimulação concluída! Resultados salvos em 'progenie_selecionada.csv'\n")
+# # Definindo o número de linhas para os data frames
+#num_linhas <- 10
+
+# # Criando o data frame do pai
+#df_pai <- data.frame(
+#  gene_A = sample(c(0, 1), num_linhas, replace = TRUE),
+#  gene_B = sample(c(0, 1), num_linhas, replace = TRUE)
+#)
+
+# # Criando o data frame da mãe
+#df_mae <- data.frame(
+#  gene_A = sample(c(0, 1), num_linhas, replace = TRUE),
+#  gene_B = sample(c(0, 1), num_linhas, replace = TRUE)
+#)
+
+# num_individuos_progênie <- 5 # Exemplo: gerar 5 novos indivíduos
+# progênie_gerada <- simular_progênie(dados_pai_exemplo, dados_mae_exemplo, num_individuos_progênie)
+
+# # Salvamento do resultado (exemplo)
+# output_filename <- "progenie_selecionada_funcao.csv"
+# write.table(
+#   progênie_gerada,
+#   file = output_filename,
+#   sep = ";",
+#   row.names = FALSE,
+#   quote = FALSE
+# )
+
+# cat(paste0("\nSimulação concluída! Resultados salvos em \'", output_filename, "\'\n"))
